e61951cdeeef977488e75b90cb58d6415c820fb8
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-02-18 00:31:46 +0700

    Make summary stronger

--- src/content-script.js
+++ src/content-script.js
@@ -1,12 +1,21 @@
-function replaceHeader() {
-    var entries = document.querySelectorAll("div.entry-content");
+function bolder(entries) {
     for(let entry of entries) {
+        if(entry.style) {
             entry.style.fontWeight = 400;
             entry.style.color = "#222";
+        }
     }
-    var comments = document.querySelector(".commentlist");
-    comments.style.fontWeight = 400;
-    comments.style.color = "#222";
+}
+
+function replaceHeader() {
+    var content = document.querySelectorAll("div.entry-content");
+    var summary = document.querySelectorAll("div.entry-summary");
+    var comment = document.querySelectorAll(".commentlist");
+
+    var all = [...content, ...summary, ...comment];
+    bolder(all);
+
+    console.log("replace ...");
 }
 
 document.addEventListener('DOMNodeInserted', replaceHeader);
